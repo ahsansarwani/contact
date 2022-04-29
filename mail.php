a8db092abb80893f7cdf1ab737f6b1cb940b4b99
Ahsan Zahoor <ahsanzahoor2@gmail.com>
2022-04-29 11:42:27 +0500

    mail

--- mail.php
+++ mail.php
@@ -6,15 +6,4 @@ $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-    return true;
-} else {
-    $is_sent = mail($to, $subject, $message, $headers);
-    if ($is_sent) {
-
-        return true;
-    } else {
-
-        return false;
-    }
-}
+mail($to, $subject, $message, $headers);
